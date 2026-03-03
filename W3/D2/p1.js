d9c756290b45a71e0fafe84c23c88033568dc4b5
Revathi-h2004 <revathishekhar25@gmail.com>
2026-03-03 15:41:27 +0530

    Arithematic Operators

---
+++ W3/D2/p1.js
@@ -0,0 +1,13 @@
+// Arithmetic Operators
+const a = 2, b = 3;
+console.log("arithematic operator");
+console.log("a+b: ",a+b);
+console.log("a-b: ",a-b);
+console.log("a*b: ",a*b);
+console.log("a/b: ",a/b);
+console.log("a%b: ",a%b); //Modules
+console.log("a**b: ",a**b); //Exponential
+console.log("a++: ",a++); post-increment
+console.log("a++: ",a++); pre-increment
+console.log("a--: ",a--);
+console.log("--a: ",--a);
