96d6a5bf9fa0d193d59e925c4e914c6bd6493f26
Revathi-h2004 <revathishekhar25@gmail.com>
2026-03-03 15:42:09 +0530

    Comparison Operators

---
+++ W3/D2/p2.js
@@ -0,0 +1,11 @@
+// Comparison operators
+let x=2, y=5;
+console.log("Comparison operators");
+console.log("x==y:" ,x==y);
+console.log("x===y:" ,x===y);
+console.log("x!=y:" ,x!=y);
+console.log("x!==y:" ,x!==y);
+console.log("x>3:" ,x>3);
+console.log("x<3:" ,x<3);
+console.log("x>=y:" ,x>=y);
+console.log("x<=y:" ,x<=y);
