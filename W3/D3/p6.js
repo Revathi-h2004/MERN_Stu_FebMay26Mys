36d698bd5ae49a66712377751f14bd87732166a5
Revathi-h2004 <revathishekhar25@gmail.com>
2026-03-04 14:00:02 +0530

    Nested object

---
+++ W3/D3/p6.js
@@ -0,0 +1,13 @@
+// Nested object
+const student = {
+    name: "Santhu",
+    score:{
+        math: 80,
+        science: 83,
+    },
+    hobbies:["reading","singing"],
+    fullname: function(){
+        
+    }
+};
+console.log(student.scores.math);
