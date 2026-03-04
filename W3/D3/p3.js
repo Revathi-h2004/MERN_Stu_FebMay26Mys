6360bcef1cce373a117e405b61ab73098d7e23d9
Revathi-h2004 <revathishekhar25@gmail.com>
2026-03-04 13:29:37 +0530

    break and continue

---
+++ W3/D3/p3.js
@@ -0,0 +1,13 @@
+for(let i=0;i<5;i++){
+    if(i===2){
+        console.log("i",i);
+        break;
+    }
+}
+// continue
+for(let i=0;i<5;i++){
+    if(i===2){
+        console.log("i",i);
+        continue;
+    }
+}
