2e8c44d2d769dc459c3a7ce80c83d250584a7391
Revathi-h2004 <revathishekhar25@gmail.com>
2026-03-09 21:45:08 +0530

    JSON Settings Merge

---
+++ W3/D3/assig3.js
@@ -0,0 +1,29 @@
+//JSON Settings Merge
+function mergeSettings(savedSettingsJSON, defaultSettings) {
+
+  //  Convert JSON string to object
+  const savedSettings = JSON.parse(savedSettingsJSON);
+
+  //  Merge objects
+  const merged = { ...defaultSettings, ...savedSettings };
+
+  // Return both object and JSON string
+  return {
+    mergedObject: merged,
+    mergedJSON: JSON.stringify(merged)
+  };
+}
+
+
+// default 
+const defaultSettings = {
+  theme: "light",
+  fontSize: 14,
+  notifications: true
+};
+
+const savedSettingsJSON = '{"theme":"dark","notifications":false}';
+
+const result = mergeSettings(savedSettingsJSON, defaultSettings);
+
+console.log(result);
