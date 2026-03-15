08bce7e7284bfe84fbd03f39720b1806a7199273
Revathi-h2004 <revathishekhar25@gmail.com>
2026-03-15 20:39:52 +0530

    Portfolio project

---
+++ W4/D5/portfolio-project/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",function(){
+    //renderskills
+    renderSkills();
+    initModal();
+    initContactValidation();
+    initThemeToggle();
+    renderProjects();
+
+
+})
